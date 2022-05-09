14d34d47d7d5a5d128a4658402845bd09d180875
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-09 11:05:26 -0700

    Update IsPalindrome.py

--- IsPalindrome/IsPalindrome.py
+++ IsPalindrome/IsPalindrome.py
@@ -16,3 +16,24 @@ class Solution(object):
                 e-=1 
                 i+=1
         return True 
+        
+        
+ class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        original = x 
+        rem = 0  
+        rev = 0  
+        if (x < 0) : 
+            return False 
+        while (x != 0) : 
+            rem = x%10 
+            rev = (rev*10)+rem
+            x/=10
+        if(original == rev): 
+            return True
+        else : 
+            return False
