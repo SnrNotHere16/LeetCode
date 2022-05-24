6bcb6ab22991f8da7579c3e07a79a6d902f2c9fb
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-24 08:11:11 -0700

    Update ReverseBits.c

--- ReverseBits/ReverseBits.c
+++ ReverseBits/ReverseBits.c
@@ -1,8 +1,9 @@
 uint32_t reverseBits(uint32_t n) {
     uint32_t r = 0; 
+	//for loop to check every bit of input and set the reversing 
     for (int i = 0; i < 32; i++){
         if (n&(UINT32_C(0x0001) << i)){
-            r |= UINT32_C(0x0001)<<(31-i);
+            r |= UINT32_C(0x0001)<<(31-i); //places a 1 on a bit. Starts from the most significant bit
         }
     }  
     return r; 
