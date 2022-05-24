07fef5cd73b27d1a97074a29c7d62d7db71e8cfa
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-23 18:03:41 -0700

    Update ReverseBits.c

--- ReverseBits/ReverseBits.c
+++ ReverseBits/ReverseBits.c
@@ -1,22 +1,14 @@
+#include <math.h>
 uint32_t reverseBits(uint32_t n) {
     uint32_t r = 0; 
-    char bin [32];
-  // printf("%x %x\n", n, n); 
     for (int i = 0; i < 32; i++){
         if (n&(UINT32_C(0x0001) << i)){
-            bin[i] = 1; 
+            r += pow(2,31-i);
         }
         else { 
-            bin[i] = 0; 
+            r += 0; 
         }
         
     }  
-    printf("\n"); 
-    for (int i = 0; i < 32; i++){
-        if (bin[31-i])
-            r|=(UINT32_C(0x0001)<<i); 
-        else 
-            r|=(UINT32_C(0x0000)<<i); 
-    }
     return r; 
 }
