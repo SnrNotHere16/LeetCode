726411a3b0ddb7e2bf8005c5896d575107d7fc41
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-23 18:11:39 -0700

    Update ReverseBits.c

--- ReverseBits/ReverseBits.c
+++ ReverseBits/ReverseBits.c
@@ -2,7 +2,7 @@ uint32_t reverseBits(uint32_t n) {
     uint32_t r = 0; 
     for (int i = 0; i < 32; i++){
         if (n&(UINT32_C(0x0001) << i)){
-            r += UINT32_C(0x0001)<<(31-i);
+            r |= UINT32_C(0x0001)<<(31-i);
         }
     }  
     return r; 
