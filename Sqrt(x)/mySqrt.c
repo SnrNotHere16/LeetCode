2922c11bab9fe92a8cefa58c08bd2dc6549997e8
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-26 13:24:45 -0700

    Update mySqrt.c

--- Sqrt(x)/mySqrt.c
+++ Sqrt(x)/mySqrt.c
@@ -1,6 +1,14 @@
+
+/*
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+*/
 int mySqrt(int x) {
 
- long  s = 1;
+ long  s = 1; //used a long because s will be squared and int is not enough 
  if (x < 2)  {
         return x;
  }
