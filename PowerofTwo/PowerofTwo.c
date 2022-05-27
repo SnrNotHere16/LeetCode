2ef8a6aab516dc5492b57289acb158079c751a9b
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-27 16:15:22 -0700

    Update PowerofTwo.c

--- PowerofTwo/PowerofTwo.c
+++ PowerofTwo/PowerofTwo.c
@@ -1,3 +1,8 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+*/
 bool isPowerOfTwo(int n) {
         int count = 0; //checks to see how many ones(binary) are in the int 
         if (n < 1)
