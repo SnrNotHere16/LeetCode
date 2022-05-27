b67954184f342e2c0450588c95768a57669c48c5
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-27 16:14:21 -0700

    Update PowerofTwo.c

--- PowerofTwo/PowerofTwo.c
+++ PowerofTwo/PowerofTwo.c
@@ -1,11 +1,13 @@
 bool isPowerOfTwo(int n) {
-        int count = 0;
+        int count = 0; //checks to see how many ones(binary) are in the int 
         if (n < 1)
                 return false;
+		//cycle through each bit
         for (int i = 0; i < 31; i++) {
                 if (n&(0x0001<<i))
                         count++;
         }
+		//if only one bit then it is a power of two
         if (count == 1)
                 return true;
         else
