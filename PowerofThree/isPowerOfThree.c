7993f5cd1701680fa4ce4bd14872fc53eb653542
SnrNotHere16 <aquineda16@hotmail.com>
2022-05-29 11:39:27 -0700

    Update isPowerOfThree.c

--- PowerofThree/isPowerOfThree.c
+++ PowerofThree/isPowerOfThree.c
@@ -1,4 +1,24 @@
 bool isPowerOfThree(int n) {
+		//Loop solution 1
+		/*
+	    int t  = 3;
+        if (n < 0 )
+                return false;
+        else if (n == 1 || n == 1162261467)
+                return true;
+        else
+                for (int i = 0; i < 18; i++) {
+                        if(n == t)
+                               return true;
+                        else
+                                t=3*t;
+
+                }
+                return false;
+				
+		*/ 
+		
+		//Loop solution 2
         int t = 1162261467;
 
         if (n < 0)
